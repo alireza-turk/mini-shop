02449b82f17b1b9043daef7d2be3c78549811357
AlirezaAhmadyan <ahmadyalireza111@gmail.com>
2024-11-06 09:40:40 +0330

    config tailwindcss & fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ export default {
   mode: 'jit',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '375px',
+      },
+      colors: {
+        red: 'hsl(14, 86%, 42%)',
+        green: 'hsl(159, 69%, 38%)',
+        'rose-50': 'hsl(20, 50%, 98%)',
+        'rose-100': 'hsl(13, 31%, 94%)',
+        'rose-300': 'hsl(14, 25%, 72%)',
+        'rose-400': 'hsl(7, 20%, 60%)',
+        'rose-500': 'hsl(12, 20%, 44%)',
+        'rose-900': 'hsl(14, 65%, 9%)',
+      },
+      fontFamily: {
+        sans: ['red-hat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
